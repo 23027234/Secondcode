c4895cb8fb0424a7e6bdb324ea7da276d4e53b09
23027234 <166645833+23027234@users.noreply.github.com>
2024-04-18 18:34:34 +0200

    Add files via upload

---
+++ Secondcode.java
@@ -0,0 +1,17 @@
+ import java.util. Scanner ;
+ public class Secondcode {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter First number");
+        int a = input.nextInt();
+
+        System.out.println("Enter Second number");
+        int b = input .nextInt();
+
+        int c = a-b;
+        
+        System.out.println("Your answer" +c);
+    }
+}
+   
